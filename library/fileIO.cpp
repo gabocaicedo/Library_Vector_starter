695604f81950d3d0b1eec33cf18f4b75fec3a019
Guest <gabriel.caicedo.17@cnu.edu>
2020-02-28 15:14:23 -0500

    Debugging gile io

--- library/fileIO.cpp
+++ library/fileIO.cpp
@@ -18,51 +18,74 @@ using namespace std;
  * 			SUCCESS if all data is loaded
  * */
 int loadBooks(std::vector<book> &books, const char *filename) {
+
 	ifstream bookistream;
 	bookistream.open(filename, ios::in); // create input stream of books from file
 										 //with name filename
 
-	if (!bookistream.is_open())
+	if (bookistream.is_open()) {
+
+		std::string book1;		//string to hold a a book that is read in
+		string bookID;
+		std::string title;
+		std::string author;
+		std::string state;
+		string ltpatronID;
+
+		book abook;
+//	book emptyBook;// book struct to be loaded with string book1 and pushed back onto vector books
+		std::stringstream ss;
+
+		while (!bookistream.eof()) {
+			getline(bookistream, book1); //get first line (first book) from file
+			ss.str(book1);
+
+			if (book1 == "") {
+				return NO_BOOKS_IN_LIBRARY;
+			}
+
+			//abook = emptyBook;	//clear vector of books
+
+			std::getline(ss, bookID, CHAR_TO_SEARCH_FOR); //get string in line up to "," and store in string bookID
+			abook.book_id = stoi(bookID); // convert bookID string to int and set book_id field of struct to converted string bookID
+
+			std::getline(ss, title, CHAR_TO_SEARCH_FOR); //get title
+			abook.title = title;
+
+			std::getline(ss, author, CHAR_TO_SEARCH_FOR); // get author
+			abook.author = author;
+
+			std::getline(ss, state, CHAR_TO_SEARCH_FOR); //get checkout state
+			switch (stoi(state)) {
+			case 0:
+				abook.state = (UNKNOWN);
+				break;
+
+			case 1:
+				abook.state = (IN);
+				break;
+
+			case 2:
+				abook.state = (OUT);
+				break;
+			}
+
+			std::getline(ss, ltpatronID, CHAR_TO_SEARCH_FOR); // get patronID that book is loaned to
+			abook.loaned_to_patron_id = stoi(ltpatronID);
+
+			books.push_back(abook); //add a book struct to book vector
+			ss.clear();			//clear ss stream reading in lines of book file
+
+		}
+		bookistream.close();
+
+	} else if (!bookistream.is_open()) {
 		return COULD_NOT_OPEN_FILE; // return could not open const if couldnt open file
-
-	if (books.size() == 0)
+	}
+	if (books.size() == 0) {
 		return NO_BOOKS_IN_LIBRARY;	// returns no books in library const if books vector is empty
-
-
-	std::string book1;		//string to hold a a book that is read in
-	std::string bookID;
-	std::string state;
-	std::string ltpatronID;
-
-	book abook;	// book struct to be loaded with string book1 and pushed back onto vector books
-	std::stringstream ss;
-
-	while (!bookistream.eof()) {
-		getline(bookistream, book1); //get first line (first book) from file
-		ss.str(book1);
-
-		books.clear();	//clear vector of books
-
-		std::getline(ss, bookID, CHAR_TO_SEARCH_FOR); //get string in line up to "," and store in string bookID
-		abook.book_id = stoi(bookID); // convert bookID string to int and set book_id field of struct to converted string bookID
-
-		std::getline(ss, abook.title, CHAR_TO_SEARCH_FOR); //get title
-
-		std::getline(ss, abook.author, CHAR_TO_SEARCH_FOR); // get author
-
-		std::getline(ss, state, CHAR_TO_SEARCH_FOR); //get checkout state
-		if (stoi(state) == 1)
-			abook.state = (IN);
-
-		std::getline(ss, ltpatronID, CHAR_TO_SEARCH_FOR); // get patronID that book is loaned to
-		abook.loaned_to_patron_id = stoi(ltpatronID);
-
-		books.push_back(abook); //add a book struct to book vector
-		ss.clear();				//clear ss stream reading in lines of book file
-
 	}
 
-	bookistream.close();
 	return SUCCESS;
 }
 
@@ -72,34 +95,37 @@ int loadBooks(std::vector<book> &books, const char *filename) {
  * 			SUCCESS if all data is saved
  * */
 int saveBooks(std::vector<book> &books, const char *filename) {
-	ofstream bookostream;
-	bookostream.open(filename, ios::in);
-	if (!bookostream.is_open())
-		return COULD_NOT_OPEN_FILE; // check if file open
-
 	if (books.size() == 0)
 		return NO_BOOKS_IN_LIBRARY; // check library
 
-	std::string book;
-	for (int i = 0; i < books.size(); ++i) {
+	ofstream bookostream;
+	bookostream.open(filename, ios::out);
 
-		book = to_string(books[i].book_id) + "," + books[i].title + ","
-				+ books[i].author + "," + to_string(books[i].state) + ","
-				+ to_string(books[i].loaned_to_patron_id); //feed all fields of book struct into a string so that we can write to file
-														   // must convert book_id, state, and loaned_to_patron_id to strings
+	if (bookostream.is_open()) {
 
-		bookostream << book << std::endl; // write back to ofstream bookostream
+		std::string book;
+		for (int i = 0; i < books.size(); ++i) {
 
-		/*vector<book>::iterator myBookVectorIterator;
-		 for(myBookVectorIterator = books.begin();
-		 myBookVectorIterator != books.end();
-		 myBookVectorIterator++){
+			book = to_string(books[i].book_id) + "," + books[i].title + ","
+					+ books[i].author + "," + to_string(books[i].state) + ","
+					+ to_string(books[i].loaned_to_patron_id); //feed all fields of book struct into a string so that we can write to file
+															   // must convert book_id, state, and loaned_to_patron_id to strings
 
-		 bookostream << myBookVectorIterator;*/
+			bookostream << book << std::endl; // write back to ofstream bookostream
 
+			/*vector<book>::iterator myBookVectorIterator;
+			 for(myBookVectorIterator = books.begin();
+			 myBookVectorIterator != books.end();
+			 myBookVectorIterator++){
+
+			 bookostream << myBookVectorIterator;*/
+
+		}
+	} else if (!bookostream.is_open()) {
+		return COULD_NOT_OPEN_FILE;
 	}
-	if (bookostream.is_open())
-		bookostream.close(); //close output stream if open
+
+	bookostream.close(); //close output stream
 	return SUCCESS;
 }
 
@@ -112,35 +138,45 @@ int loadPatrons(std::vector<patron> &patrons, const char *filename) {
 	ifstream patronistream;
 	patronistream.open(filename, ios::in);
 
-	if (!patronistream.is_open())
-		return COULD_NOT_OPEN_FILE; // return could not open constant if couldn't open input patron file
+	if (patronistream.is_open()) {
+
+		std::string patron1;
+		std::string patronID;
+		std::string patronName;
+		std::string numbooks;
+		patron apatron;
+		std::stringstream ss;
+
+		while (!patronistream.eof()) {
+			getline(patronistream, patron1);
+			ss.str(patron1);
+
+			if(patron1 == ""){
+				return NO_PATRONS_IN_LIBRARY;
+			}
+
+			//	patrons.clear();
+
+			std::getline(ss, patronID, CHAR_TO_SEARCH_FOR);
+			apatron.patron_id = stoi(patronID);
+
+			std::getline(ss, patronName, CHAR_TO_SEARCH_FOR);
+			apatron.name = patronName;
+
+			std::getline(ss, numbooks, CHAR_TO_SEARCH_FOR);
+			apatron.number_books_checked_out = stoi(numbooks);
+
+			patrons.push_back(apatron);
+			ss.clear();
+		}
+		patronistream.close();
+
+	} else if (!patronistream.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	}
 
 	if (patrons.size() == 0) {
 		return NO_PATRONS_IN_LIBRARY; // returns no patrons in library constant if patron vector is empty
-	}
-
-	std::string patron1;
-	std::string patronID;
-	std::string numbooks;
-	patron apatron;
-	std::stringstream ss;
-
-	while (!patronistream.eof()) {
-		getline(patronistream, patron1);
-		ss.str(patron1);
-
-		patrons.clear();
-
-		std::getline(ss, patronID, CHAR_TO_SEARCH_FOR);
-		apatron.patron_id = stoi(patronID);
-
-		std::getline(ss, apatron.name, CHAR_TO_SEARCH_FOR);
-
-		std::getline(ss, numbooks, CHAR_TO_SEARCH_FOR);
-		apatron.number_books_checked_out = stoi(numbooks);
-
-		patrons.push_back(apatron);
-		ss.clear();
 	}
 
 	return SUCCESS;
@@ -153,7 +189,7 @@ int loadPatrons(std::vector<patron> &patrons, const char *filename) {
  * */
 int savePatrons(std::vector<patron> &patrons, const char *filename) {
 	ofstream patronostream;
-	patronostream.open(filename, ios::in);
+	patronostream.open(filename, ios::out);
 	if (!patronostream.is_open())
 		return COULD_NOT_OPEN_FILE;
 
--- library/library.cpp
+++ library/library.cpp
@@ -13,11 +13,25 @@ using namespace std;
 //NOTE: also make sure you save patron and book data to disk any time you make a change to them
 //NOTE: for files where data is stored see constants.h BOOKFILE and PATRONFILE
 
+//vectors for holding patron and book objects
+vector<book> bookVector;
+vector<patron> patronVector;
+
+//pointers to input book and patron files
+const char *bookFile = BOOKFILE.c_str();
+const char *patronFile = PATRONFILE.c_str();
+const char *emptyBookFile = BOOKFILE_EMPTY.c_str();
+const char *emptyPatronFile = PATRONFILE_EMPTY.c_str();
 /*
  * clear books and patrons containers
  * then reload them from disk 
  */
-void reloadAllData(){
+void reloadAllData() {
+	bookVector.clear();
+	patronVector.clear(); //clear vectors
+
+	loadBooks(bookVector, bookFile);
+	loadPatrons(patronVector, patronFile);
 
 }
 
@@ -41,7 +55,53 @@ void reloadAllData(){
  * 		   BOOK_NOT_IN_COLLECTION
  *         TOO_MANY_OUT patron has the max number of books allowed checked out
  */
-int checkout(int bookid, int patronid){
+int checkout(int bookid, int patronid) {
+	reloadAllData();
+
+	int bookCheck;
+	int patronCheck; //temporary variables that hold value returned from checking if patron/book is in vector
+
+	for (int i = 0; i < bookVector.size(); i++) {
+		if (bookid == bookVector[i].book_id) {
+			bookCheck = i;
+			break;
+		}
+		bookCheck = BOOK_NOT_IN_COLLECTION;
+	}
+
+	if (bookCheck == BOOK_NOT_IN_COLLECTION) {
+		return BOOK_NOT_IN_COLLECTION;
+	}
+	book checkedBook = bookVector[bookCheck];
+
+	for (int i = 0; i < patronVector.size(); i++) {
+		if (patronid == patronVector[i].patron_id) { // patron is enrolled, break from loop
+			patronCheck = i;
+			break;
+		}
+		patronCheck = PATRON_NOT_ENROLLED;
+	}
+
+	if (patronCheck == PATRON_NOT_ENROLLED) {
+		return PATRON_NOT_ENROLLED; // patron not enrolled if patron_id not in patron vector
+
+	}
+	patron checkedPatron = patronVector[patronCheck];
+
+	if (checkedPatron.number_books_checked_out == MAX_BOOKS_ALLOWED_OUT) {
+		return TOO_MANY_OUT;
+	}
+
+	checkedBook.state = OUT;
+	checkedBook.loaned_to_patron_id = patronid;
+	checkedPatron.number_books_checked_out++;  //update fields of both structs
+
+	saveBooks(bookVector, bookFile);
+	savePatrons(patronVector, patronFile); // save books and patrons
+
+	bookVector.clear();
+	patronVector.clear(); // clear vectors for next use
+
 	return SUCCESS;
 }
 
@@ -57,7 +117,38 @@ int checkout(int bookid, int patronid){
  * returns SUCCESS checkout worked
  * 		   BOOK_NOT_IN_COLLECTION
  */
-int checkin(int bookid){
+int checkin(int bookid) {
+	reloadAllData();
+
+	int bookCheck = 0;
+	int findPatron = 0;
+
+	for (int i = 0; i < bookVector.size(); i++) {
+		if (bookid == bookVector[i].book_id) {
+			bookCheck = i;
+			break;
+		}
+		bookCheck = BOOK_NOT_IN_COLLECTION;
+	}
+
+	if (bookCheck == BOOK_NOT_IN_COLLECTION) {
+		return BOOK_NOT_IN_COLLECTION;
+	}
+	book checkedBook = bookVector[bookCheck];
+
+	findPatron = bookVector[bookCheck].loaned_to_patron_id;
+	patron foundPatron = patronVector[findPatron]; // find patron checking in book
+
+	checkedBook.state = IN;
+	checkedBook.loaned_to_patron_id = NO_ONE;
+	foundPatron.number_books_checked_out--;
+
+	saveBooks(bookVector, bookFile);
+	savePatrons(patronVector, patronFile); // save books and patrons
+
+	bookVector.clear();
+	patronVector.clear(); // clear vectors for next use
+
 	return SUCCESS;
 }
 
@@ -70,8 +161,22 @@ int checkin(int bookid){
  * return 
  *    the patron_id of the person added
  */
-int enroll(std::string &name){
-	return 0;
+int enroll(std::string &name) {
+	reloadAllData();
+
+	patron aPatron;
+	aPatron.name = name;
+	aPatron.number_books_checked_out = NONE;
+
+	patronVector.push_back(aPatron);
+
+	saveBooks(bookVector, bookFile);
+	savePatrons(patronVector, patronFile); // save books and patrons
+
+	bookVector.clear();
+	patronVector.clear(); // clear vectors for next use
+
+	return numbPatrons() + 1; // return id of new patron
 }
 
 /*
@@ -79,16 +184,16 @@ int enroll(std::string &name){
  * (ie. if 3 books returns 3)
  * 
  */
-int numbBooks(){
-	return 0;
+int numbBooks() {
+	return bookVector.size();
 }
 
 /*
  * the number of patrons in the patrons container
  * (ie. if 3 patrons returns 3)
  */
-int numbPatrons(){
-	return 0;
+int numbPatrons() {
+	return patronVector.size();
 }
 
 /*the number of books patron has checked out
@@ -96,8 +201,26 @@ int numbPatrons(){
  *returns a positive number indicating how many books are checked out 
  *        or PATRON_NOT_ENROLLED         
  */
-int howmanybooksdoesPatronHaveCheckedOut(int patronid){
+int howmanybooksdoesPatronHaveCheckedOut(int patronid) {
+	reloadAllData();
 
+	int patronCheck = 0;
+
+	for (int i = 0; i < patronVector.size(); i++) {
+		if (patronid == patronVector[i].patron_id) { // patron is enrolled, break from loop
+			patronCheck = i;
+			break;
+		}
+		patronCheck = PATRON_NOT_ENROLLED;
+	}
+
+	if (patronCheck == PATRON_NOT_ENROLLED) {
+		return PATRON_NOT_ENROLLED; // patron not enrolled if patron_id not in patron vector
+
+	}
+	patron checkedPatron = patronVector[patronCheck];
+
+	return checkedPatron.number_books_checked_out;
 
 }
 
@@ -107,7 +230,26 @@ int howmanybooksdoesPatronHaveCheckedOut(int patronid){
  * returns SUCCESS found it and name in name
  *         PATRON_NOT_ENROLLED no patron with this patronid
  */
-int whatIsPatronName(std::string &name,int patronid){
-	return SUCCESS;
+int whatIsPatronName(std::string &name, int patronid) {
+	reloadAllData();
+
+	int patronCheck = 0;
+
+	for (int i = 0; i < patronVector.size(); i++) {
+		if (patronid == patronVector[i].patron_id) { // patron is enrolled, break from loop
+			patronCheck = i;
+			break;
+		}
+		patronCheck = PATRON_NOT_ENROLLED;
+	}
+
+	if (patronCheck == PATRON_NOT_ENROLLED) {
+		return PATRON_NOT_ENROLLED; // patron not enrolled if patron_id not in patron vector
+
+	}
+
+	patronVector[patronCheck].name = name;
+
+	return patronid;
 }
 
